face9d08843aa11d4e7a1cc84eaf62dfd0698273
Caio de la Rosa <caio.gomiero@hotmail.com>
2022-03-20 19:49:06 -0300

    criando classe Posicao

---
+++ ProjetoXadrez/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tabuleiro;
+
+namespace ProjetoXadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + p);
+
+        }
+    }
+}
